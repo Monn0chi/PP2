f091b01b670740f67c80b27026b00c28ec2dcf82
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-02-03 11:29:16 +0500

    Add main folder practice_2

---
+++ practice_2/boolean/boolean_comparison.py
@@ -0,0 +1,9 @@
+print(5 == 5)
+
+print(10 != 3)
+
+print(7 >= 2)
+
+print(13 < 5)
+
+print(1 > 0)
---
+++ practice_2/boolean/boolean_intro.py
@@ -0,0 +1,9 @@
+is_student = True
+print(is_student)
+
+is_raining = False
+print(is_raining)
+
+a = 5
+b = 10
+print(a < b)
