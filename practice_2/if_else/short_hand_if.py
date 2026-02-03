3bba2da9463741df9d37e21a275e4d3d6526e2b6
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-02-03 11:36:59 +0500

    Add short_hand_if.py file and write few example

---
+++ practice_2/if_else/short_hand_if.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+print("a is bigger") if a > b else print("b is bigger")
+
+x = 7
+print("Even") if x % 2 == 0 else print("Odd")
+
+is_logged = True
+print("Welcome") if is_logged else print("Please login")
