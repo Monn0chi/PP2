b0fe4928c83e9bd58bb410d75325b17e2d7d5f98
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-02-03 11:36:03 +0500

    Add if_elif_else.py file and write few example

---
+++ practice_2/if_else/if_elif_else.py
@@ -0,0 +1,23 @@
+score = 85
+if score >= 90:
+    print("A")
+elif score >= 70:
+    print("B")
+else:
+    print("C")
+
+x = 0
+if x > 0:
+    print("Positive")
+elif x < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+age = 15
+if age < 13:
+    print("Child")
+elif age < 18:
+    print("Teen")
+else:
+    print("Adult")
