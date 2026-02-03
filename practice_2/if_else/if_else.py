945c20c2bf6244b1b4292250b2f167fdc487b05a
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-02-03 11:36:31 +0500

    Add if_else.py file and write few example

---
+++ practice_2/if_else/if_else.py
@@ -0,0 +1,17 @@
+x = 3
+if x > 5:
+    print("Big")
+else:
+    print("Small")
+
+number = 10
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+password = "1234"
+if password == "1234":
+    print("Access granted")
+else:
+    print("Access denied")
