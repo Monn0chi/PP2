8e0a9f954f2699daa82f490188f04e30da4f5f3a
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-02-19 09:36:14 +0500

    Add return_values.py file

---
+++ practice3/functions/return_values.py
@@ -0,0 +1,19 @@
+def greetings(name):
+    return f"Hello {name}, how are you"
+
+resust = greetings("Beksultan")
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+
+def calculate(a, b):
+    sum_value = a + b
+    product = a * b
+    return sum_value, product
+
+result = calculate(4, 5)
+print(result)
