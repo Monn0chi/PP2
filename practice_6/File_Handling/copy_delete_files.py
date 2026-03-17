1b0108c6fac21fd3df037da389d2dca01294eb69
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-03-17 10:53:44 +0500

    Practice_6 add File_Handling with a .py examples

---
+++ practice_6/File_Handling/copy_delete_files.py
@@ -0,0 +1,5 @@
+
+import os
+
+if os.path.exists("backup.txt"):
+    os.remove("backup.txt")
---
+++ practice_6/File_Handling/write_files.py
@@ -0,0 +1,3 @@
+with open("data.txt", "w") as file:
+    file.write("Hello\n")
+    file.write("This is a test file\n")
